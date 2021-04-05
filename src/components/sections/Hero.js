5975f4a9dc695fc65c40445fc1fd5eb13187137f
Gurpinder Singh Sandhu <gpsinghsandhu@yahoo.com>
2021-04-05 21:28:51 +0530

    Made changes to content

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,20 +61,20 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Your problems our <span className="text-color-primary">solutions</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
-                </p>
-              <div className="reveal-from-bottom" data-reveal-delay="600">
+                Animo Labs is a a team of experienced Software Engineers based out of New Delhi, India. We have over the years focused our expertise on creating End-to-End technology solutions for various businesses around the world.
+              </p>
+              <div className="reveal-from-bottom" data-reveal-delay="500">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
-                    Get started
+                  <Button tag="a" color="primary" wideMobile href="/#/contact-us">
+                    Contact Us
                     </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
+                  {/* <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
                     View on Github
-                    </Button>
+                    </Button> */}
                 </ButtonGroup>
               </div>
             </div>
