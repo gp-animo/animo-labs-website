7431b2f060fb284a3b7b3dd01d0f010b413cc64d
Gurpinder Singh Sandhu <gpsinghsandhu@yahoo.com>
2021-04-05 21:10:42 +0530

    Used hashRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import { createHashHistory } from "history";
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 //import './App.css';
 import './assets/scss/style.scss';
 
-const history = createBrowserHistory();
+const history = createHashHistory();
 
 ReactDOM.render(
   <Router history={history}>
@@ -21,4 +21,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
